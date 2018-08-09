1248aad2523c5fc7cb37298e294fbbfd2f6dfb17
David Librera <davidlibrera@gmail.com>
2018-08-09 17:39:38 +0200

    LogoutRequest attributes

--- lib/spid/saml2/logout_request.rb
+++ lib/spid/saml2/logout_request.rb
@@ -3,9 +3,12 @@
 module Spid
   module Saml2
     class LogoutRequest # :nodoc:
+      attr_reader :settings
+      attr_reader :uuid
       attr_reader :document
 
-      def initialize(uuid: nil)
+      def initialize(uuid: nil, settings:)
+        @settings = settings
         @document = REXML::Document.new
         @uuid = uuid || SecureRandom.uuid
       end
@@ -24,15 +27,27 @@ module Spid
           end
       end
 
+      # rubocop:disable Metrics/MethodLength
       def logout_request_attributes
         @logout_request_attributes ||=
           begin
             attributes = {
               "xmlns:samlp" => "urn:oasis:names:tc:SAML:2.0:protocol",
-              "xmlns:saml" => "urn:oasis:names:tc:SAML:2.0:assertion"
+              "xmlns:saml" => "urn:oasis:names:tc:SAML:2.0:assertion",
+              "ID" => "_#{uuid}",
+              "Version" => "2.0",
+              "IssueInstant" => issue_instant,
+              "Destination" => settings.idp_entity_id
             }
             attributes
           end
+      end
+      # rubocop:enable Metrics/MethodLength
+
+      private
+
+      def issue_instant
+        @issue_instant ||= Time.now.utc.iso8601
       end
     end
   end
--- spec/spid/saml2/logout_request_spec.rb
+++ spec/spid/saml2/logout_request_spec.rb
@@ -4,10 +4,29 @@ require "spec_helper"
 
 RSpec.describe Spid::Saml2::LogoutRequest do
   subject(:logout_request) do
-    described_class.new
+    described_class.new(
+      uuid: "unique-uuid",
+      settings: settings
+    )
+  end
+
+  let(:settings) do
+    instance_double(
+      "Spid::Saml2::Settings",
+      idp_entity_id: "https://identity.provider"
+    )
   end
 
   it { is_expected.to be_a described_class }
+
+  before do
+    Timecop.freeze
+    Timecop.travel("2018-08-04 01:00 +01:00")
+  end
+
+  after do
+    Timecop.return
+  end
 
   describe "#to_saml" do
     let(:saml_message) { logout_request.to_saml }
@@ -21,6 +40,15 @@ RSpec.describe Spid::Saml2::LogoutRequest do
 
       it "exists" do
         expect(node).not_to be_nil
+      end
+
+      {
+        "ID" => "_unique-uuid",
+        "Version" => "2.0",
+        "IssueInstant" => "2018-08-04T00:00:00Z",
+        "Destination" => "https://identity.provider"
+      }.each do |name, value|
+        include_examples "has attribute", name, value
       end
     end
   end
