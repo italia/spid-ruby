2d06ff7ebb59d82921a1bc0181e71cba17d2c88e
David Librera <davidlibrera@gmail.com>
2018-08-09 17:24:16 +0200

    Moved shared examples for attributes under spec/support

--- spec/spid/saml2/authn_request_spec.rb
+++ spec/spid/saml2/authn_request_spec.rb
@@ -3,23 +3,6 @@
 require "spec_helper"
 
 RSpec.describe Spid::Saml2::AuthnRequest do
-  shared_examples "has attribute" do |name, value|
-    it "contains '#{name}' attribute" do
-      attribute = node.attribute(name)
-
-      expect(attribute).not_to be_nil
-      expect(attribute.value).to eq value
-    end
-  end
-
-  shared_examples "hasn't attribute" do |name|
-    it "doens't contain '#{name}' attribute" do
-      attribute = node.attribute(name)
-
-      expect(attribute).to be_nil
-    end
-  end
-
   subject(:authn_request) do
     described_class.new(
       uuid: "unique-uuid",
