16c53daa069ab65a3c41a42d074a4fe114f86045
David Librera <davidlibrera@gmail.com>
2018-08-10 17:23:52 +0200

    Entity descriptior node init

--- lib/spid/saml2/sp_metadata.rb
+++ lib/spid/saml2/sp_metadata.rb
@@ -3,6 +3,34 @@
 module Spid
   module Saml2
     class SPMetadata # :nodoc:
+      attr_reader :document
+      attr_reader :settings
+
+      def initialize(settings:)
+        @document = REXML::Document.new
+        @settings = settings
+      end
+
+      def to_saml
+        document.add_element(entity_descriptor)
+        document.to_s
+      end
+
+      def entity_descriptor
+        @entity_descriptor ||=
+          begin
+            element = REXML::Element.new("md:EntityDescriptor")
+            element.add_attributes(entity_descriptor_attributes)
+            element
+          end
+      end
+
+      def entity_descriptor_attributes
+        @entity_descriptor_attributes ||= {
+          "xmlns:md" => "urn:oasis:names:tc:SAML:2.0:metadata",
+          "EntityID" => settings.sp_entity_id
+        }
+      end
     end
   end
 end
--- spec/spid/saml2/sp_metadata_spec.rb
+++ spec/spid/saml2/sp_metadata_spec.rb
@@ -3,9 +3,36 @@
 require "spec_helper"
 
 RSpec.describe Spid::Saml2::SPMetadata do
-  subject do
-    described_class.new
+  subject(:sp_metadata) do
+    described_class.new(settings: settings)
+  end
+
+  let(:settings) do
+    instance_double(
+      "Spid::Saml2::Settings",
+      sp_entity_id: "https://service.provider"
+    )
   end
 
   it { is_expected.to be_a described_class }
+
+  describe "#to_saml" do
+    let(:saml_message) { sp_metadata.to_saml }
+
+    let(:xml_document) { REXML::Document.new(saml_message) }
+
+    let(:node) do
+      xml_document.elements[xpath]
+    end
+
+    describe "md:EntityDescriptor node" do
+      let(:xpath) { "/md:EntityDescriptor" }
+
+      it "exists" do
+        expect(node).not_to be_nil
+      end
+
+      include_examples "has attribute", "EntityID", "https://service.provider"
+    end
+  end
 end
