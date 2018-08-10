5a055df87cce610d389cee0504c827f4e8abf176
David Librera <davidlibrera@gmail.com>
2018-08-10 16:08:56 +0200

    Empty sp_metadata class

--- lib/spid/saml2.rb
+++ lib/spid/saml2.rb
@@ -5,6 +5,7 @@ require "spid/saml2/identity_provider"
 require "spid/saml2/settings"
 require "spid/saml2/authn_request"
 require "spid/saml2/logout_request"
+require "spid/saml2/sp_metadata"
 require "spid/saml2/utils"
 
 module Spid
---
+++ lib/spid/saml2/sp_metadata.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module Spid
+  module Saml2
+    class SPMetadata # :nodoc:
+    end
+  end
+end
