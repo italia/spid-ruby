e4503ed92e380046ccf7860c592b390a9aa1704b
David Librera <davidlibrera@gmail.com>
2018-08-30 12:07:16 +0200

    Init class IdpLogoutResponse

---
+++ lib/spid/saml2/idp_logout_response.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module Spid
+  module Saml2
+    class IdpLogoutResponse # :nodoc:
+    end
+  end
+end
---
+++ spec/spid/saml2/idp_logout_response_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Spid::Saml2::IdpLogoutResponse do
+  subject(:response) do
+    described_class.new
+  end
+
+  it { is_expected.to be_a described_class }
+end
