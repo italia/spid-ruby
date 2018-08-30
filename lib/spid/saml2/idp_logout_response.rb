aabb6cf388fdf283b59929bda6f05600e516e9fb
David Librera <davidlibrera@gmail.com>
2018-08-30 12:23:32 +0200

    Logout Response and Issuer on response

--- lib/spid/saml2/idp_logout_response.rb
+++ lib/spid/saml2/idp_logout_response.rb
@@ -3,6 +3,48 @@
 module Spid
   module Saml2
     class IdpLogoutResponse # :nodoc:
+      attr_reader :document
+      attr_reader :settings
+
+      def initialize(settings:)
+        @document = REXML::Document.new
+        @settings = settings
+      end
+
+      def to_saml
+        document.add_element(logout_response)
+        document.to_s
+      end
+
+      def logout_response
+        @logout_response ||=
+          begin
+            element = REXML::Element.new("samlp:LogoutResponse")
+            element.add_attributes(logout_response_attributes)
+            element.add_element(issuer)
+            element
+          end
+      end
+
+      def logout_response_attributes
+        @logout_response_attributes ||= {
+          "xmlns:samlp" => "urn:oasis:names:tc:SAML:2.0:protocol",
+          "xmlns:saml" => "urn:oasis:names:tc:SAML:2.0:assertion"
+        }
+      end
+
+      def issuer
+        @issuer ||=
+          begin
+            element = REXML::Element.new("saml:Issuer")
+            element.add_attributes(
+              "Format" => "urn:oasis:names:tc:SAML:2.0:nameid-format:entity",
+              "NameQualifier" => settings.sp_entity_id
+            )
+            element.text = settings.sp_entity_id
+            element
+          end
+      end
     end
   end
 end
--- spec/spid/saml2/idp_logout_response_spec.rb
+++ spec/spid/saml2/idp_logout_response_spec.rb
@@ -4,8 +4,39 @@ require "spec_helper"
 
 RSpec.describe Spid::Saml2::IdpLogoutResponse do
   subject(:response) do
-    described_class.new
+    described_class.new(settings: settings)
+  end
+
+  let(:settings) do
+    instance_double(
+      "Spid::Saml2::Settings",
+      sp_entity_id: "https://service.provider"
+    )
   end
 
   it { is_expected.to be_a described_class }
+
+  describe "#to_saml" do
+    let(:saml_message) { response.to_saml }
+
+    let(:xml_document) { REXML::Document.new(saml_message) }
+
+    let(:node) { xml_document.elements[xpath] }
+
+    describe "samlp:LogoutResponse" do
+      let(:xpath) { "/samlp:LogoutResponse" }
+
+      it "exists" do
+        expect(node).not_to be_nil
+      end
+
+      describe "saml:Issuer" do
+        let(:xpath) { super() + "/saml:Issuer" }
+
+        it "exists" do
+          expect(node).not_to be_nil
+        end
+      end
+    end
+  end
 end
