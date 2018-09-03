2d79d03e535ebe619c5467d296cf2460d718970d
David Librera <davidlibrera@gmail.com>
2018-09-03 21:53:24 +0200

    initializer configuration

---
+++ lib/spid/generators.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require "rails/generators"
+require "spid/generators/install_generator"
+
+module Spid
+  module Generators # :nodoc:
+  end
+end
--- lib/spid/railtie.rb
+++ lib/spid/railtie.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "spid/generators"
+
 module Spid
   class Railtie < ::Rails::Railtie # :nodoc:
     rake_tasks do |_app|
